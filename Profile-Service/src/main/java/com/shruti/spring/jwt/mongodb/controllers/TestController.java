af998017caec2332d1ef91566f3f2bc6fa3c037e
shruti1999799 <70285813+shruti1999799@users.noreply.github.com>
2021-10-03 17:04:00 +0530

    Add files via upload

---
+++ Profile-Service/src/main/java/com/shruti/spring/jwt/mongodb/controllers/TestController.java
@@ -0,0 +1,60 @@
+package com.shruti.spring.jwt.mongodb.controllers;
+
+import java.util.List;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shruti.spring.jwt.mongodb.models.User;
+import com.shruti.spring.jwt.mongodb.repository.UserRepository;
+
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+	private final UserRepository userRepository;
+	public TestController(UserRepository userRepository) {
+		this.userRepository=userRepository;
+	}
+	
+	@GetMapping("/all")
+	public String allAccess() {
+		return "Public Content.";
+	}
+	
+	@GetMapping("/user")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+	public String userAccess() {
+		return "User Content.";
+	}
+
+	@GetMapping("/mod")
+	@PreAuthorize("hasRole('MODERATOR')")
+	public String moderatorAccess() {
+		return "Moderator Board.";
+	}
+
+	@GetMapping("/admin")
+	@PreAuthorize("hasRole('ADMIN')")
+	public String adminAccess() {
+		return "Admin Board.";
+	}
+	
+	@GetMapping("/allUsers")
+	public List<User> getUsers(){
+		return userRepository.findAll();
+	}
+	
+	
+	@DeleteMapping("/delete/{id}")
+	public void deleteProduct(@PathVariable String id) {
+		  userRepository.deleteById(id);
+		
+	}
+}
